9451e5f06323d07512827cea1fe9dc7d4dcd2f99
erstert3st <67842643+erstert3st@users.noreply.github.com>
2022-04-12 21:58:40 +0200

    Update ___init___.py

--- ___init___.py
+++ ___init___.py
@@ -1,32 +1,21 @@
 # virtualenv venv
 #source venv/bin/activate
 #folder_exists
-
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import io
-import logging
 import os
-from textwrap import wrap
 
-import octoprint.filemanager
+#import octoprint.filemanager
 import octoprint.filemanager.storage
-import octoprint.plugin
-from octoprint import UMASK
+
+#import octoprint.plugin
 from octoprint.filemanager.destinations import FileDestinations
 from octoprint.filemanager.util import StreamWrapper
-from octoprint.util import atomic_write
 
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.AssetPlugin):
-
-
-                       
-
         # **Icon
         # **Revision 
         # ***add configure may for Que addon 
@@ -56,45 +45,33 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
         # C++ Make Confuigure 
         # Recive 
         #  Print 
-        
-  
-
     def on_after_startup(self):
         
         Filemanager = octoprint.filemanager.storage.LocalFileStorage(
             self.get_plugin_data_folder()
         )
 
-
-        for _ in range(2):
-            self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
         #breakpoint()
         print('\x1b[6;30;42m' + 'Spacer--------______________________________!' + '\x1b[0m')
-            #impl_storage
         files = self._file_manager.list_files(FileDestinations.LOCAL)
-        #self._logger.info(files)
-        #self._logger.info(files.get('local'))
         fileNameList = [*files.get('local').keys()]
         #filenames = dictFilenames() ## should remove dict
         self._logger.info(fileNameList)
         fileNameDict = dict(file0 = {'filename': 'examplename', 'command': 'example40001', 'icon': 'shouldbeimplemented'})
-        counter = 1
-        for fileName in fileNameList:
-            strCounter = str(counter)
+        for itr, fileName in enumerate(fileNameList):
+            strCounter = str(itr)
             fileNameDict['file' + strCounter] = {'filename': fileName, 'command': 'example4000' + strCounter, 'icon': 'shouldbeimplemented'}
-            counter = counter + 1
         fileNameDict.pop('file0')
 
         #self._logger.info(fileNameDict)
-        #list done 
-        #make Folder
+        #list done  #make Folder
         path = self.get_plugin_data_folder() +"/readyFiles"
         self._logger.info(path)
         if not Filemanager.folder_exists(path):
             print('\x1b[6;30;42m' + 'in if :D' + '\x1b[0m')
             Filemanager.add_folder(path)
+        
         #add Files
-
         file_obj = StreamWrapper(
             os.path.basename(path),
             io.BytesIO(
@@ -103,14 +80,12 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                 ).encode("ascii", "replace")
             )
         )
-        counter = 1
         for fId, fItem in fileNameDict.items():
             pathWithFile = path + '/' +fItem['filename']
             if not Filemanager.file_exists(pathWithFile):
                 Filemanager.add_file(pathWithFile, file_object=file_obj,  allow_overwrite=False, display=None)
                 print('\x1b[6;30;42m' + fId + ' wrote'+ '\x1b[0m')
 
-           # self._basefolder
         print('\x1b[6;30;42m' + "done" + '\x1b[0m')
  
     def get_settings_defaults(self):
@@ -120,9 +95,7 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
     def get_template_configs(self):
         for _ in range(10):
             self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
-       # breakpoint()
         print('\x1b[6;30;42m' + 'Spacer--------______________________________!' + '\x1b[0m')
-
 
 
         return [
